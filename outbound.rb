007858f20e7e56ec8765a5c515e64d19a93ecc53
Gimenez Silva German Alberto <ggerman@ggermanlp.ggerman.net>
2014-08-14 14:42:56 -0300

    Write result in a file

--- outbound.rb
+++ outbound.rb
@@ -7,37 +7,56 @@ require 'open-uri'
 require 'uri'
 
 class Outbound
+  
+  def head
+    return "<html>
+	    <head>
+	      <title>Domain Outbound Links Check</title>
+	    </head>
+	    <body><h1>Domain Outbound Links Check</h1>"
+  end
+
+  def footer
+    return "</body>
+	    </html>"
+  end  
+  
   def resume (url)
     unless url.empty?
+      html = nil
       hostname = URI(url)
       Anemone.crawl(url) do |anemone|
 	anemone.on_every_page do |page|
 	  if page.url.to_s.include? hostname.host.to_s and page.url.to_s.include? "http://"
-	      puts "<h1>#{page.url}</h1>"
+	      html = "<h2>#{page.url}</h2>"
 	      
 	      begin
 		body = Nokogiri::HTML(open(page.url))
 		
-		puts '<ul>'
+		html = html + '<ul>'
 		body.css("body a").map do |link|
 		  unless link['href'].to_s.include? hostname.host.to_s
 		    if link['href'].to_s.include? "http"
-		      puts "<li>#{link['href']}</li>"
+		      html = html + "<li>#{link['href']}</li>"
 		    end
 		  end
 		end
-		puts '</ul>'
+		html = html + '</ul>'
 	      rescue OpenURI::HTTPError => error
 		response = error.io
-		puts "<li>#{page.url} - #{response.status}</li>"
+		html = html + "<li>#{page.url} - #{response.status}</li>"
 	      end
 	      end
 	end
       end
       
     end
+    return self.head + html + self.footer
   end
+  
 end
 
 link = Outbound.new
-link.resume ARGV[0]
+
+file = File.open "log_001.html", "w"
+file.write link.resume ARGV[0]
